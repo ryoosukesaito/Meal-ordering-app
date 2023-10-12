073fef01c8cac015516239344d65050be0fc1d65
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-10-12 13:42:17 -0700

    modified import type of firebase sdk on backend

--- backend/src/firebaseBackend.ts
+++ backend/src/firebaseBackend.ts
@@ -1,13 +1,20 @@
+import fs from 'fs'
+import path from 'path'
+
 import admin from 'firebase-admin'
 
-import * as serviceAccount from '../fb-adminsdk.cjs'
-// const serviceAccount = JSON.parse(fs.readFileSync('./fb-adminsdk.json', 'utf8'))
-
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: 'https://ordering-meal-app.firebaseio.com'
-})
+//set try and catch since using readFileSync
+try {
+	const serviceAccount = JSON.parse(
+		fs.readFileSync(path.resolve(__dirname, '../fb-adminsdk.json'), 'utf8')
+	)
+	admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount),
+		databaseURL: 'https://ordering-meal-app.firebaseio.com'
+	})
+} catch (error) {
+	console.error('something went wrong in firebaseBackend', error)
+}
 
 const AdminDB = admin.firestore()
-
 export { AdminDB }
